a0028745d633494cbbf2289fd62b23fcb316ed82
Tiger <skdlghwns@gmail.com>
2023-10-27 23:44:23 +0900

    23_10_27 거듭제곱수 만들기

--- src/main/java/implement/num10965/Solution.java
+++ src/main/java/implement/num10965/Solution.java
@@ -1,80 +1,66 @@
 package implement.num10965;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-import java.io.FileInputStream;
-import java.util.Stack;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
- */
+
 class Solution
 {
+    static int capacity = 10000001;
+    static boolean[] sieve = new boolean[capacity];
     public static void main(String args[]) throws Exception
     {
-		/*
-		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
-		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
-		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
-		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
-		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
-		 */
-        //System.setIn(new FileInputStream("res/input.txt"));
+        List<Integer> decimal = new ArrayList<>();
+        for(int i=2; i<=Math.sqrt(capacity); i++){
+            if(sieve[i]){
+                continue;
+            }
+            decimal.add(i);
+            if(Math.sqrt(i)%1 != 0){
+                for(int j=i*2; j<capacity; j +=i){
+                    if((j)%i==0){
+                        sieve[j]=true;
+                    }
+                }
+            }
+        }
 
-		/*
-		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
-		 */
         Scanner sc = new Scanner(System.in);
         int T;
         T=sc.nextInt();
-        Stack<Integer> value = new Stack<>();
-        int[] tmp = new int[T];
         int n;
-        int answer;
-        int preValue=0;
-        int nowValue;
         int[] answerArr = new int[T];
-		/*
-		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-		*/
-        for(int test_case = 0; test_case <T; test_case++)
-        {
-            tmp[test_case] = sc.nextInt();
-        }
+
         for(int test_case = 0; test_case < T; test_case++)
         {
-//            n = sc.nextInt();
-            n = tmp[test_case];
-            answer = 1;
-            while(n!=1){
-                for(int i=2; i<=n; i++){
-                    if(n%i==0){
-                        n=n/i;
-                        value.push(i);
-                        break;
-                    }
+            n = sc.nextInt();
+            answerArr[test_case]=1;
+
+            if(!sieve[n]){
+                answerArr[test_case]=n;
+                continue;
+            }
+
+            for(int i : decimal){
+                if(i>n){
+                    break;
+                }
+                int count = 0;
+                while (n%i==0){
+                    n/=i;
+                    count++;
+                }
+                if(count%2==1){
+                    answerArr[test_case] *= i;
+                }
+                if(!sieve[n]){
+                    answerArr[test_case] *= n;
+                    break;
                 }
             }
-            while(!value.empty()){
-                nowValue = value.pop();
-                if(preValue!=nowValue){
-                    if(value.empty()) {
-                        answer *=nowValue;
-                        break;
-                    }
-                    if(preValue==0){
-                        preValue=nowValue;
-                        continue;
-                    }
-                    answer *= preValue;
-                    preValue=nowValue;
-                    continue;
-                }
-                preValue=0;
-            }
-            answerArr[test_case] = answer;
-            preValue=0;
         }
+
         for(int i=0; i<answerArr.length;i++){
             System.out.printf("#%d %d\n", i+1, answerArr[i]);
         }
